9d1b7b3bfee7baa931f5fb818f1856ed01a4a0ad
luisalejandrovm <LuisAlejandroVM@hotmail.com>
2021-09-09 23:09:18 -0500

    Initial commit

---
+++ src/main/java/mx/edu/utez/client/JavaClient.java
@@ -0,0 +1,21 @@
+package mx.edu.utez.client;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class JavaClient {
+    public static void main(String[] args) throws MalformedURLException, XmlRpcException {
+        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+        config.setServerURL(new URL("http://localhost:1200"));
+        XmlRpcClient client = new XmlRpcClient();
+        client.setConfig(config);
+        Object[] params = {1, 4};
+        int result = (Integer) client.execute("Handler.suma" , params);
+
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/mx/edu/utez/server/Handler.java
@@ -0,0 +1,7 @@
+package mx.edu.utez.server;
+
+public class Handler {
+    public int suma(int x, int y){
+        return x + y;
+    }
+}
